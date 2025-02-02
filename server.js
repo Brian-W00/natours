6e1cee0009960ae8cd4e8d0cbe986d2a9c851bfb
Brian-W00 <ertongwei@uchicago.edu>
2025-02-02 13:11:00 -0600

    null

--- server.js
+++ server.js
@@ -22,6 +22,6 @@ mongoose
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
-    console.log(`App running on port ${port}`);
+    console.log(`App running on port 1 ${port}`);
 });
 
